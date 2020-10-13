3bcd07e08477fba07915cb9f42dc1a6c9ff61319
woods <han-xuefeng@hotmail.com>
2020-10-13 18:28:47 +0800

    恢复调试代码

--- config/routes.php
+++ config/routes.php
@@ -1,18 +1,7 @@
 <?php
 
-//use App\Controller\HelloController;
-//
-//return [
-//    ['GET', '/hello/index', [HelloController::class,'index']]
-//];
-
 use App\Controller\HelloController;
-use Rebuild\HttpServer\Router\Router;
 
-Router::addRoute(['GET', 'POST', 'HEAD'], '/', [HelloController::class, 'index']);
-Router::addRoute(['GET', 'POST', 'HEAD'], '/demo', [HelloController::class, 'demo']);
-
-Router::addGroup('/hello', function() {
-    Router::addRoute(['GET', 'POST', 'HEAD'], '/test', [HelloController::class, 'test']);
-    Router::addRoute(['GET', 'POST', 'HEAD'], '/test2', [HelloController::class, 'test2']);
-});
+return [
+    ['GET', '/hello/index', [HelloController::class,'index']]
+];
--- src/HttpServer/Router/DispatcherFactory.php
+++ src/HttpServer/Router/DispatcherFactory.php
@@ -5,10 +5,9 @@ namespace Rebuild\HttpServer\Router;
 
 use FastRoute\DataGenerator\GroupCountBased as DataGenerator;
 use FastRoute\Dispatcher;
-use FastRoute\Dispatcher\GroupCountBased;
 use FastRoute\RouteCollector;
 use FastRoute\RouteParser\Std;
-//use function FastRoute\simpleDispatcher;
+use function FastRoute\simpleDispatcher;
 
 class DispatcherFactory
 {
@@ -37,37 +36,23 @@ class DispatcherFactory
     public function getDispatcher(string $serverName): Dispatcher
     {
         if (! isset($this->dispatcher[$serverName])) {
-//            $this->dispatcher[$serverName] = simpleDispatcher(function (RouteCollector $r) {
-//                foreach ($this->routes as $route) {
-//                    [$method, $path, $handle] = $route;
-//                    $r->addRoute($method, $path, $handle);
-//                }
-//            });
-            $router = $this->getRouter($serverName);
-            $this->dispatcher[$serverName] = new GroupCountBased($router->getData());
-
+            $this->dispatcher[$serverName] = simpleDispatcher(function (RouteCollector $r) {
+                foreach ($this->routes as $route) {
+                    [$method, $path, $handle] = $route;
+                    $r->addRoute($method, $path, $handle);
+                }
+            });
         }
         return $this->dispatcher[$serverName];
     }
     public function initConfigRoute()
     {
-        Router::init($this);
         foreach ($this->routeFile as $file) {
             if (file_exists($file)){
-                require_once $file;
-//                $route = require_once $file;
-//                $this->routes = array_merge_recursive($this->routes,$route);
+                $route = require_once $file;
+                $this->routes = array_merge_recursive($this->routes,$route);
             }
         }
     }
 
-    public function getRouter(string $serverName): RouteCollector
-    {
-        if (isset($this->routers[$serverName])) {
-            return $this->routers[$serverName];
-        }
-        $parser =  new Std();
-        $generator = new DataGenerator();
-        return $this->routers[$serverName] = new RouteCollector($parser, $generator, $serverName);
-    }
 }
